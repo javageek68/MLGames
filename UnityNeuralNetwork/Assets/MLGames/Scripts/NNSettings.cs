5f6b53b4900a0c1cd18bcc7e0de5d28b8bee99a9
McGeek <mikebrascome@gmail.com>
2020-09-03 11:15:57 -0500

    add serializer and settings

---
+++ UnityNeuralNetwork/Assets/MLGames/Scripts/NNSettings.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLGames
+{ 
+    public class NNSettings
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public enum ActivationFunctions
+        {
+            sigmoid = 0,
+            tanh = 1,
+            relu = 2,
+            leakyrelu = 3
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int[] layers { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public float[][] neurons { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public float[][] biases { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public float[][][] weights { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public int[] activations { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ActivationFunctions[] layerActivations { get; set; }
+
+        public NNSettings()
+        {
+           
+        }
+    }
+}
