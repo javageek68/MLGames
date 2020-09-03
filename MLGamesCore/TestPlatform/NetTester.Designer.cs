ce88fa8c3f6e3d77d5baa18b91e9c14d0018e7fa
McGeek <mikebrascome@gmail.com>
2020-09-03 09:05:47 -0500

    started work on test platform

---
+++ MLGamesCore/TestPlatform/NetTester.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace TestPlatform
+{
+    partial class NetTester
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Black;
+            this.btnStart.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnStart.Location = new System.Drawing.Point(18, 24);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(102, 28);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtStatus.BackColor = System.Drawing.Color.Black;
+            this.txtStatus.ForeColor = System.Drawing.Color.White;
+            this.txtStatus.Location = new System.Drawing.Point(20, 75);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(763, 363);
+            this.txtStatus.TabIndex = 1;
+            // 
+            // NetTester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnStart);
+            this.Name = "NetTester";
+            this.Text = "NetTester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtStatus;
+    }
+}
---
+++ MLGamesCore/TestPlatform/NetTester.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MLGames;
+using static MLGames.NeuralNetwork;
+
+namespace TestPlatform
+{
+    public partial class NetTester : Form
+    {
+        NeuralNetwork net;
+        int[] layers = new int[3] { 3, 5, 1 };
+
+        public ActivationFunctions[] activation = new ActivationFunctions[2] { ActivationFunctions.leakyrelu, ActivationFunctions.leakyrelu };
+        public NetTester()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+
+
+        void Start()
+        {
+            this.net = new NeuralNetwork(layers, activation);
+            for (int i = 0; i < 20000; i++)
+            {
+                net.BackPropagate(new float[] { 0, 0, 0 }, new float[] { 0 });
+                net.BackPropagate(new float[] { 1, 0, 0 }, new float[] { 1 });
+                net.BackPropagate(new float[] { 0, 1, 0 }, new float[] { 1 });
+                net.BackPropagate(new float[] { 0, 0, 1 }, new float[] { 1 });
+                net.BackPropagate(new float[] { 1, 1, 0 }, new float[] { 1 });
+                net.BackPropagate(new float[] { 0, 1, 1 }, new float[] { 1 });
+                net.BackPropagate(new float[] { 1, 0, 1 }, new float[] { 1 });
+                net.BackPropagate(new float[] { 1, 1, 1 }, new float[] { 1 });
+            }
+            this.Display("cost: " + net.cost);
+
+            Display(net.FeedForward(new float[] { 0, 0, 0 })[0].ToString());
+            Display(net.FeedForward(new float[] { 1, 0, 0 })[0].ToString());
+            Display(net.FeedForward(new float[] { 0, 1, 0 })[0].ToString());
+            Display(net.FeedForward(new float[] { 0, 0, 1 })[0].ToString());
+            Display(net.FeedForward(new float[] { 1, 1, 0 })[0].ToString());
+            Display(net.FeedForward(new float[] { 0, 1, 1 })[0].ToString());
+            Display(net.FeedForward(new float[] { 1, 0, 1 })[0].ToString());
+            Display(net.FeedForward(new float[] { 1, 1, 1 })[0].ToString());
+            //We want the gate to simulate 3 input or gate (A or B or C)
+            // 0 0 0    => 0
+            // 1 0 0    => 1
+            // 0 1 0    => 1
+            // 0 0 1    => 1
+            // 1 1 0    => 1
+            // 0 1 1    => 1
+            // 1 0 1    => 1
+            // 1 1 1    => 1
+        }
+
+        void Display(string strMsg)
+        {
+            this.txtStatus.AppendText(strMsg + "\r\n");
+        }
+    }
+}
