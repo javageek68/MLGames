2b061dd2d609636fbca45e6ca24f75b238b9b0ab
James Michael Brascome <mikebrascome@gmail.com>
2020-09-08 17:12:40 -0500

    adding grid to network designer

--- MLGamesCore/TestPlatform/Games/NetworkDesigner.Designer.cs
+++ MLGamesCore/TestPlatform/Games/NetworkDesigner.Designer.cs
@@ -30,6 +30,11 @@
         {
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.dvGrid = new System.Windows.Forms.DataGridView();
@@ -58,10 +63,22 @@
             // 
             // dvGrid
             // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Silver;
+            this.dvGrid.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dvGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dvGrid.BackgroundColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dvGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dvGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dvGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.LayerName,
@@ -71,19 +88,19 @@
             this.dvGrid.Location = new System.Drawing.Point(8, 48);
             this.dvGrid.MultiSelect = false;
             this.dvGrid.Name = "dvGrid";
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dvGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
-            this.dvGrid.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dvGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.Color.White;
+            this.dvGrid.RowsDefaultCellStyle = dataGridViewCellStyle7;
             this.dvGrid.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.Black;
             this.dvGrid.RowTemplate.DefaultCellStyle.ForeColor = System.Drawing.Color.White;
             this.dvGrid.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.DimGray;
@@ -92,17 +109,29 @@
             // 
             // LayerName
             // 
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.Silver;
+            this.LayerName.DefaultCellStyle = dataGridViewCellStyle3;
             this.LayerName.HeaderText = "Layer Name";
             this.LayerName.Name = "LayerName";
             this.LayerName.ReadOnly = true;
             // 
             // Size
             // 
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.Silver;
+            this.Size.DefaultCellStyle = dataGridViewCellStyle4;
             this.Size.HeaderText = "Size";
             this.Size.Name = "Size";
             // 
             // Activation
             // 
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.Silver;
+            this.Activation.DefaultCellStyle = dataGridViewCellStyle5;
             this.Activation.HeaderText = "Activation";
             this.Activation.Name = "Activation";
             // 
@@ -117,7 +146,7 @@
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.Color.White;
             this.Name = "NetworkDesigner";
-            this.Text = "NetworkDesigner";
+            this.Text = "Network Designer";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dvGrid)).EndInit();
             this.ResumeLayout(false);
--- MLGamesCore/TestPlatform/Games/NetworkDesigner.cs
+++ MLGamesCore/TestPlatform/Games/NetworkDesigner.cs
@@ -11,15 +11,24 @@ using System.Windows.Forms;
 
 namespace TestPlatform.Games
 {
+    
     public partial class NetworkDesigner : Form
     {
+        string strActivationNone = "none";
+
+        string strColSize = "Size";
+        string strColActivation = "Activation";
+
         //NeuralNetwork nn = new NeuralNetwork()
         public NetworkDesigner()
         {
             InitializeComponent();
             this.Load += NetworkDesigner_Load;
+            this.dvGrid.CellValueChanged += DvGrid_CellValueChanged;
             
         }
+
+    
 
         private void NetworkDesigner_Load(object sender, EventArgs e)
         {
@@ -36,7 +45,12 @@ namespace TestPlatform.Games
             DataTable dataTable = new DataTable();
             dataTable.Columns.Add(strName);
             dataTable.Columns.Add(strValue);
-            int intRowNo = 0;
+            DataRow firstrow = dataTable.NewRow();
+            firstrow[strName] = strActivationNone;
+            firstrow[strValue] = 0;
+            dataTable.Rows.Add(firstrow);
+
+            int intRowNo = 1;
             foreach (var strFunction in Enum.GetValues(typeof(NNSettings.ActivationFunctions)))
             {
                 DataRow row = dataTable.NewRow();
@@ -47,6 +61,49 @@ namespace TestPlatform.Games
             }
 
             this.Activation.DataSource = dataTable;
+        }
+
+        private void DvGrid_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            //if (this.dvGrid.CurrentRow != null)
+            //{
+            //    DataGridViewRow dgRow = this.dvGrid.CurrentRow;
+            //    string strSize = dgRow.Cells["Size"].Value.ToString();
+            //    string strActivation = dgRow.Cells["Activation"].Value.ToString();
+            //}
+            this.SaveGridData();
+        }
+
+        private void SaveGridData()
+        {
+            
+            List<int> lstLayers = new List<int>();
+            List<NNSettings.ActivationFunctions> lstAct = new List<NNSettings.ActivationFunctions>();
+            foreach(DataGridViewRow row in this.dvGrid.Rows)
+            {
+                if (row.Cells[this.strColSize].Value == null || row.Cells[this.strColActivation].Value == null) return;
+
+                //get layer size from grid
+                string strSize = row.Cells[this.strColSize].Value.ToString();
+                //add layer size to the list
+                lstLayers.Add(int.Parse(strSize));
+
+                //get the index of the activation from the grid
+                string strActIndex = row.Cells[this.strColActivation].Value.ToString();
+                //convert to in and subject 1 to account for the "none" in the first position
+                //note that the user should select "none" for the input layer
+                int intActIndex = int.Parse(strActIndex) - 1;
+                //if the user did not select "none", then add it to the list
+                if (intActIndex > -1)
+                {
+                    NNSettings.ActivationFunctions activationFunction = (NNSettings.ActivationFunctions)intActIndex;
+                    lstAct.Add(activationFunction);
+                }
+             
+            }
+            int[] layers = lstLayers.ToArray();
+            NNSettings.ActivationFunctions[] actFuncts = lstAct.ToArray();
+
         }
     }
 }
