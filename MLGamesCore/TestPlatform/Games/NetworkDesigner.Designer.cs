dbf1b9df96bbdf490d471f508005b6ad5917ca3b
James Michael Brascome <mikebrascome@gmail.com>
2020-09-08 15:57:11 -0500

    started work on netwrok designer

---
+++ MLGamesCore/TestPlatform/Games/NetworkDesigner.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace TestPlatform.Games
+{
+    partial class NetworkDesigner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dvGrid = new System.Windows.Forms.DataGridView();
+            this.LayerName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Size = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Activation = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dvGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Layers";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(76, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(117, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // dvGrid
+            // 
+            this.dvGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dvGrid.BackgroundColor = System.Drawing.Color.Black;
+            this.dvGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.LayerName,
+            this.Size,
+            this.Activation});
+            this.dvGrid.GridColor = System.Drawing.Color.Gray;
+            this.dvGrid.Location = new System.Drawing.Point(8, 48);
+            this.dvGrid.MultiSelect = false;
+            this.dvGrid.Name = "dvGrid";
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dvGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            this.dvGrid.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dvGrid.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.Black;
+            this.dvGrid.RowTemplate.DefaultCellStyle.ForeColor = System.Drawing.Color.White;
+            this.dvGrid.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.DimGray;
+            this.dvGrid.Size = new System.Drawing.Size(485, 185);
+            this.dvGrid.TabIndex = 3;
+            // 
+            // LayerName
+            // 
+            this.LayerName.HeaderText = "Layer Name";
+            this.LayerName.Name = "LayerName";
+            this.LayerName.ReadOnly = true;
+            // 
+            // Size
+            // 
+            this.Size.HeaderText = "Size";
+            this.Size.Name = "Size";
+            // 
+            // Activation
+            // 
+            this.Activation.HeaderText = "Activation";
+            this.Activation.Name = "Activation";
+            // 
+            // NetworkDesigner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(519, 259);
+            this.Controls.Add(this.dvGrid);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "NetworkDesigner";
+            this.Text = "NetworkDesigner";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dvGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DataGridView dvGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LayerName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Size;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Activation;
+    }
+}
---
+++ MLGamesCore/TestPlatform/Games/NetworkDesigner.cs
@@ -0,0 +1,52 @@
+﻿using MLGames;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestPlatform.Games
+{
+    public partial class NetworkDesigner : Form
+    {
+        //NeuralNetwork nn = new NeuralNetwork()
+        public NetworkDesigner()
+        {
+            InitializeComponent();
+            this.Load += NetworkDesigner_Load;
+            
+        }
+
+        private void NetworkDesigner_Load(object sender, EventArgs e)
+        {
+            this.PopulationActivationDDL();
+        }
+
+        private void PopulationActivationDDL()
+        {
+            string strName = "Name";
+            string strValue = "Value";
+            this.Activation.ValueMember = strValue;
+            this.Activation.DisplayMember = strName;
+
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add(strName);
+            dataTable.Columns.Add(strValue);
+            int intRowNo = 0;
+            foreach (var strFunction in Enum.GetValues(typeof(NNSettings.ActivationFunctions)))
+            {
+                DataRow row = dataTable.NewRow();
+                row[strName] = strFunction.ToString();
+                row[strValue] = intRowNo;
+                dataTable.Rows.Add(row);
+                intRowNo++;
+            }
+
+            this.Activation.DataSource = dataTable;
+        }
+    }
+}
