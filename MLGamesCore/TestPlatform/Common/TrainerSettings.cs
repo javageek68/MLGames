39f45b2172edfeb8de50281dfa147ea00165ccd6
James Michael Brascome <mikebrascome@gmail.com>
2020-09-18 10:35:11 -0500

    reorganize source code

--- MLGamesCore/TestPlatform/Common/TrainerSettings.cs
+++ MLGamesCore/TestPlatform/Common/TrainerSettings.cs
@@ -7,6 +7,12 @@ using static MLGames.NNSettings;
 
 namespace TestPlatform.Common
 {
+    //TODO: Add max generations to settings
+    //TODO: Add plateau detection to trainer
+    //TODO: Add worker server logic - the server logic.  the main server adds configurations to try.  the workers select configuration, run them and report the results
+    //TODO: Add ML optimizer to the server to select which configuration to try next
+    //TODO: Port non UI classes over to the Unity project
+
     /// <summary>
     /// 
     /// </summary>
